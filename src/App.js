ca6e179daf5db86fdc9106c85de24c21dda82671
Aakash <aakashanand1995@gmail.com>
2023-06-01 15:14:00 +0530

    removed unneeded items

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import TextForm from "./components/textform";
 function App() {
   return (
     <div className="App">
-      <Navbar Title="Homepage" />
-      <Header Heading="Text Analyzer" />
+      <Navbar Title="Text Analyzer" />
+      <Header Heading="Enter the Text" />
       <TextForm Number="10" />
       <Button />
     </div>
